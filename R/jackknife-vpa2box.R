b803761f7f8b3e13c4b1b7711f3ff478d414a162
lauriekell <laurie@kell.es>
2018-01-26 07:14:36 +0000

    tidy

--- R/jackknife-vpa2box.R
+++ R/jackknife-vpa2box.R
@@ -56,19 +56,34 @@ saveQ<-function(i,dgs,dir){
   
   write.table(cbind(iter=i,res),file=file.path(dir,"q.txt"),append=TRUE,row.names=FALSE,col.names=FALSE)
   }
+
+saveDiags<-function(i,dgs,dir){
+  res=diags.vpa2box(dgs)
   
+  write.table(cbind(iter=i,res),file=file.path(dir,"diags.txt"),append=TRUE,row.names=FALSE,col.names=FALSE)
+  }
+
 jackknife.vpa2box<-function(file,m=0.2){
   
   sink("/dev/null")
   
   path=getPath(file)
   ctl =getFile(file)
+  where=getwd()
   setwd(getPath(file))
   fls =vpa2boxFiles(file)
   
   #so can replace latter
   dFl=paste(fls[1],"_",sep="")
   file.copy(fls[1],dFl)
+  
+  ids=readVPA2BoxIndices(fls[1])
+  
+  ## index for observations
+  obs=ldply(idx[-length(idx)],function(x) t(unlist(strsplit(x,"\t"))[1:2]))
+  names(obs)=c("index","year")
+  obs=cbind(iter=seq(dim(obs)[1]),obs)
+  obs=transform(obs,name=unique(q$name)[index])
   
   d  =scan(dFl,sep="\n",what=as.character("a"))
   ln =seq(length(d))[substr(d,1,2)=="-1"]
@@ -79,17 +94,19 @@ jackknife.vpa2box<-function(file,m=0.2){
   
   dirTmp= tempdir() 
   system(paste("wine vpa-2box.exe", ctl))
-  saveNF(0,fls[5],dirTmp)
-  saveQ(0,fls[3],dirTmp)
+  saveNF(   0,fls[5],dirTmp)
+  saveQ(    0,fls[3],dirTmp)
   
   m_ply(data.frame(i=seq(length(idx)-1)), function(i){
     cat(d1,     file=fls[1],sep="\n")
     cat(idx[-i],file=fls[1],sep="\n",append=TRUE)
     cat(d2,     file=fls[1],sep="\n",append=TRUE)
     system(paste("wine vpa-2box.exe", ctl))
-    saveNF(i,file.path(path,fls[5]),dirTmp)
-    saveQ( i,file.path(path,fls[3]),dirTmp)
+    saveNF(   i,file.path(path,fls[5]),dirTmp)
+    saveQ(    i,file.path(path,fls[3]),dirTmp)
     })
+  
+  nms=c("error","iter",names(diags.vpa2box(file.path(path,fls[3]))))
   
   file.remove(fls[1])
   file.copy(dFl,fls[1])
@@ -100,12 +117,23 @@ jackknife.vpa2box<-function(file,m=0.2){
   yf=rbind(data.frame(name=0,year=0),yf)
   dimnames(yf)[[1]]=c(0,seq(dim(yf)[1]-1))
   
-  n =read.table(file.path(dirTmp,"n.txt"))
-  n =as.FLQuant(transmute(melt(n,id=c("V1","V2")),age=as.numeric(variable),data=value,year=V2,iter=V1))
-  f =read.table(file.path(dirTmp,"f.txt"))
-  f =as.FLQuant(transmute(melt(f,id=c("V1","V2")),age=as.numeric(variable),data=value,year=V2,iter=V1))
-  q =read.table(file.path(dirTmp,"q.txt"))
-  
+  n  =read.table(file.path(dirTmp,"n.txt"))
+  n  =as.FLQuant(transmute(melt(n,id=c("V1","V2")),age=as.numeric(variable),data=value,year=V2,iter=V1))
+  f  =read.table(file.path(dirTmp,"f.txt"))
+  f  =as.FLQuant(transmute(melt(f,id=c("V1","V2")),age=as.numeric(variable),data=value,year=V2,iter=V1))
+
+  ## index for observations
+  obs=ldply(idx[-length(idx)],function(x) t(unlist(strsplit(x,"\t"))[1:2]))
+  names(obs)=c("index","year")
+  obs=cbind(iter=seq(dim(obs)[1]),obs)
+  obs=transform(obs,name=unique(q$name)[index])[,-2]
+ 
+  q  =read.table(file.path(dirTmp,"q.txt"))
+  names(q)=c("iter","name","q")
+  q=subset(q,iter!=0)
+  q=merge(obs,q)
+  q=q[order(q$iter),]
+
   file.remove(file.path(dirTmp,"n.txt"))
   file.remove(file.path(dirTmp,"f.txt"))
   file.remove(file.path(dirTmp,"q.txt"))
@@ -128,9 +156,31 @@ jackknife.vpa2box<-function(file,m=0.2){
   rfs=stk
   rfs@stock.n=stock.n(iter(rfs,-1))
   rfs@harvest=harvest(iter(rfs,-1))
-
+  
+  setwd(where)
+  
   sink(NULL)
  
   return(list(stock=FLStocks("fit"=rf1,"jackknife"=rfs),q=q))}
+
+
+run.vpa2box<-function(file,m=0.2){
+  
+  sink("/dev/null")
+  
+  path =getPath(file)
+  ctl  =getFile(file)
+  where=getwd()
+  setwd(getPath(file))
+  
+  dirTmp= tempdir() 
+  system(paste("wine vpa-2box.exe", ctl))
+  res=readVPA2Box(file,m=m)
+              
+  setwd(where)
+  
+  sink(NULL)
+  
+  return(res)}
 
 
